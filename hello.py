de0cd247b7d21cfd156d7af9633c98b5781048bb
Nate Dillon <natedillon@gmail.com>
2015-05-07 16:03:33 -0500

    The URL Patterns

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
+from django.conf.urls import url
 from django.http import HttpResponse
 
 
+# View
 def index(request):
 	return HttpResponse('Hello World')
+
+
+# URLs
+urlpatterns = (
+	url(r'^$', index)
+)
