dc7c3f92fe1d6e4f516380508e2bff3612123e0c
Nate Dillon <natedillon@gmail.com>
2015-05-07 16:21:37 -0500

    Running the Example

--- hello.py
+++ hello.py
@@ -1,20 +1,9 @@
+import sys
+
+
 from django.conf import settings
-from django.conf.urls import url
-from django.http import HttpResponse
 
 
-# View
-def index(request):
-	return HttpResponse('Hello World')
-
-
-# URLs
-urlpatterns = (
-	url(r'^$', index)
-)
-
-
-# Settings
 settings.configure(
 	DEBUG = True,
 	SECRET_KEY = 'thisisthesecretkey',
@@ -25,3 +14,22 @@ settings.configure(
 		'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	),
 )
+
+
+from django.conf.urls import url
+from django.http import HttpResponse
+
+
+def index(request):
+	return HttpResponse('Hello World')
+
+
+urlpatterns = (
+	url(r'^$', index)
+)
+
+
+if __name__ == "__main__":
+	from django.core.management import execute_from_command_line
+
+	execute_from_command_line(sys.argv)
