4a72146976200c5daf910cf5750f93383aa6ec97
Nate Dillon <natedillon@gmail.com>
2015-05-11 10:23:12 -0500

    Additional Configuration

--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
+import os
 import sys
-
 
 from django.conf import settings
 
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 settings.configure(
-	DEBUG = True,
-	SECRET_KEY = 'thisisthesecretkey',
+	DEBUG = DEBUG,
+	SECRET_KEY = SECRET_KEY,
+	ALLOWED_HOSTS = ALLOWED_HOSTS,
 	ROOT_URLCONF = __name__,
 	MIDDLEWARE_CLASSES = (
 		'django.middleware.common.CommonMiddleware',
@@ -14,7 +20,6 @@ settings.configure(
 		'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	),
 )
-
 
 from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
