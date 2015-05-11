a4b18fe79023fb711f8d96ccd9d42933de449978
Nate Dillon <natedillon@gmail.com>
2015-05-11 15:52:46 -0500

    Placeholder View

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -21,13 +21,29 @@ settings.configure(
 	),
 )
 
+
+from django import forms
 from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+
+
+class ImageForm(forms.Form):
+	"""Form to validate requested placeholder image."""
+
+	height = forms.IntegerField(min_value=1, max_value=2000)
+	width = forms.IntegerField(min_value=1, max_value=2000)
 
 
 def placeholder(request, width, height):
-	return HttpResponse('Ok')
+	form = ImageForm({'height': height, 'width': width})
+	if form.is_valid():
+		height = form.cleaned_data['height']
+		width = form.cleaned_data['width']
+		return HttpResponse('Ok')
+	else:
+		return HttpResponseBadRequest('Invalid Image Request')
+
 
 def index(request):
 	return HttpResponse('Hello World')
